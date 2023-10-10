a3677eb6238a16339c1d46525f77b8739b78d204
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-10 20:32:56 -0300

    feat(Create files): The edit and Update were included in the controllers and the views were added to the edit and _form folder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,23 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
       render :new
+    end
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render :edit
+      end
     end
   end
 
