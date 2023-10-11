d7ba45291c5dec36cb39bb7a983944cacbcfe0f2
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-11 05:54:50 -0300

    feat: In this commit, the page footer and header were created along with some styles and add gem rails-i18n witn settings

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Blogstrap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
