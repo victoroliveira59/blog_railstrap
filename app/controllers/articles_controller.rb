7e85f503407f1c6e58e44b81b3748b577a2e8bc8
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-13 05:56:27 -0300

    perf(Condition): The condition that was in the action in the controller was replaced and added to the model.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,16 +6,11 @@ class ArticlesController < ApplicationController
     @highlights = Article.desc_order.first(3)
 
     current_page = (params[:page] || 1).to_i
+    highlights_ids = @highlights.pluck(:id).join(',')
 
-    if @highlights.present?
-      highlights_ids = @highlights.pluck(:id).join(',')
-      @articles = Article.without_highlights(highlights_ids)
-                         .desc_order
-                         .page(current_page)
-    else
-      @articles = Article.desc_order
-                         .page(current_page)
-    end
+    @articles = Article.without_highlights(highlights_ids)
+                       .desc_order
+                       .page(current_page)
   end
 
   def show; end
