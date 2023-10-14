c770e9c658527bfd734042eaf42f298cd834e8aa
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-14 20:54:58 -0300

    feat: Added 'belongs_to :category' to establish the association of articles with their category.
    Indicates a one-to-many connection with another model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :category
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
 
