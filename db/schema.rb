84faedc5df03710237e6c59a8fa122fc7e4990bd
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-14 20:25:30 -0300

    feat: "Creating a category scaffold and a migration with a reference to Articles."

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_012951) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_090032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_012951) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "articles", "categories"
 end
