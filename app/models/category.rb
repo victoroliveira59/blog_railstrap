bc2ec4b748c0462608b5f6d9f4d359f88c0e759c
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-15 16:23:42 -0300

    fix: "A spelling error in the model/categories was corrected."

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,5 +5,5 @@ class Category < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3 }
 
-  scope :desc_order, -> { order(:name) } #lista os nomes em ordem alfabética
+  scope :sorted, -> { order(:name) } #lista os nomes em ordem alfabética
 end
